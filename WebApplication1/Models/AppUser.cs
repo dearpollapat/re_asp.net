65ba8d8d9d49c71d74b8bfbc2ced0233f32b0ecb
dearpollapat <pollapat.dear@gmail.com>
2024-12-13 11:21:13 +0700

    create models

---
+++ WebApplication1/Models/AppUser.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace WebApplication1.Models
+{
+    public class AppUser
+    {
+        public int? Pace { get; set; }
+        public int? Mileage { get; set; }
+        public Address? Address { get; set; }
+        public ICollection<Club> Clubs { get; set; }
+        public ICollection<Race> Races { get; set; }
+    }
+}
